004cafc061102b98792549b40d3e9d3a2b859a1f
Rasmus Krogsgaard Vester <124626087+Trlton@users.noreply.github.com>
2024-10-03 11:26:38 +0200

    Optimized timing (works)

--- JoystickMap+DriftCorrection.py
+++ JoystickMap+DriftCorrection.py
@@ -1,4 +1,9 @@
 import serial
+from djitellopy import tello
+import time
+
+drone = tello.Tello()
+
 
 # Replace with your Arduino serial port
 SERIAL_PORT = 'COM10'
@@ -24,15 +29,17 @@ def read_data_from_arduino():
                 x = int(x_str)
                 y = int(y_str)
 
-    
+                # Apply drift compensation
+                y_compensated = y - DRIFT_COMPENSATION
 
                 # Map joystick values from 63-190 to desired range (e.g., -100 to 100)
                 x_mapped = map_value(x, 258, 764, -100, 100)  # Example range -100 to 100
                 y_mapped = int(2+(map_value(y, 258, 762, -102, 101)))  # Example range -100 to 100
 
                 # Print mapped values or send to drone
-                print(f"X: {x_mapped}, Y: {y_mapped}")
 
 
+                drone.send_rc_control(y_mapped,x_mapped,0,0)
+                time.sleep(0.1)
 if __name__ == "__main__":
     read_data_from_arduino()
